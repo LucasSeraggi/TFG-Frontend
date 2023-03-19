ee93864821c2fe248033748d4439db5b67a740a2
Lucas Seraggi <lucas.seraggi@hotmail.com>
2023-03-18 21:05:20 -0300

    Pagina de cadastro de usuario e outros

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginPage from './pages/LoginPage';
-import RegisterSchool from './pages/login/RegisterSchool';
+import LoginPage from './pages/Login/LoginPage';
+import RegisterSchool from './pages/Register/RegisterSchool';
+import RegisterUser from './pages/Register/RegisterUser';
 
 export default function RouterPage() {
   return (
@@ -8,6 +9,7 @@ export default function RouterPage() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/sign-up/school" element={<RegisterSchool />} />
+        <Route path="/sign-up/user" element={<RegisterUser />} />
       </Routes>
     </BrowserRouter>
   );
